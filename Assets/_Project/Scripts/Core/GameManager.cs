d3c5340ce0f747acd448ebb87833a0eee2f6819c
MMikilanjelo <144453227+MMikilanjelo@users.noreply.github.com>
2024-09-07 00:28:26 +0300

    Added Foundation for future Game Structure

---
+++ Assets/_Project/Scripts/Core/GameManager.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Game.Core {
+	public class GameManager : Singleton<GameManager> {
+
+		public event Action OnUpdate = delegate { };
+
+		protected override void Awake() {
+			base.Awake();
+		}
+
+		/// <summary>
+		///Only subscribe for On Update Event never put you code into Update Method From other class and here
+		/// </summary>
+		public void Update() {
+			OnUpdate?.Invoke();
+		}
+	}
+
+}
